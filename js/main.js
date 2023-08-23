cb977fdf18620fbfa853da0e733ce7e208efab4c
Fede Latu <fedelatu@hotmail.com>
2023-08-23 16:09:27 -0300

    funciones

--- js/main.js
+++ js/main.js
@@ -1,15 +1,69 @@
-let taller = parseInt (prompt("¿Requiere 1.Taller 2.Productos?"))
-if (taller <2 ) {
-    alert ("Bienvenido, visite el apartado de Taller para contactarse por el servicio requerido")
-    
-} 
-else {
-    alert ("Bienvenido, visite el apartado de Productos para ver nuestro stock disponible") 
+const servicios = [
+    {id: 1, nombre: "centrado de llanta", precio: 1500},
+    {id: 2, nombre: "cambio de maza", precio: 3100},
+    {id: 3, nombre: "cambio de eje", precio: 1100},
+    {id: 4, nombre: "parchado", precio: 700},
+];
+
+
+const carrito = [];
+
+
+let opciones = "Seleccione una opción (0 en caso de no precisar):\n";
+
+for (let i = 0; i < servicios.length; i++) {
+    opciones += servicios[i].id + ". " + servicios[i].nombre + "\n";
 }
 
-do { 
-    producto = prompt("¿Qué producto/servicio requiere?");
-    continuar = prompt("¿Desea comprar algo más?");
+
+let opcionElegida = parseInt(prompt(opciones));
+
+
+if (opcionElegida >= 1 && opcionElegida <= servicios.length) {
+    let servicioElegido = null;
+    for (let i = 0; i < servicios.length; i++) {
+        if (servicios[i].id === opcionElegida) {
+            servicioElegido = servicios[i];
+            break;
+        }
+    }
+
     
-} while (continuar === "si");
-alert("Gracias por visitar Rodados Kharuby")
+    if (servicioElegido) {
+        let cantidad = parseFloat(prompt("Indique la cantidad de unidades requeridas para " + servicioElegido.nombre + ":"));
+
+        // Verificar la cantidad ingresada
+        if (!isNaN(cantidad) && cantidad > 0) {
+            carrito.push({ servicio: servicioElegido, cantidad });
+            alert(cantidad + " unidades de " + servicioElegido.nombre + " añadidas al carrito.");
+        } else {
+            alert("Cantidad inválida. Ingrese una cantidad válida.");
+        }
+    } else {
+        alert("Opción inválida. Elija una opción válida.");
+    }
+} else {
+    alert("No eligió ningún servicio.");
+}
+
+
+let carritoTexto = "Productos en el carrito:\n";
+
+for (let i = 0; i < carrito.length; i++) {
+    carritoTexto += carrito[i].cantidad + " unidades de " + carrito[i].servicio.nombre + " - Precio unitario: $" + carrito[i].servicio.precio + "\n";
+}
+
+
+alert(carritoTexto);
+
+
+const preciofiltrado = 2800;
+function filtrarpormenorprecio(servicios,preciofiltrado ){
+    const encontrado = servicios.find((el) => el.precio <= preciofiltrado);
+    return encontrado
+}
+
+
+//console.log(carrito);
+//const total = carrito.reduce((acc, el) == acc + el.precio, 0);
+//console.log(total);
